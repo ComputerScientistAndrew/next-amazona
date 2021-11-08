623fd11c4c7e5e51cfd590a34b42c6bafc8d1dd7
ComputerScientistAndrew <44506286+ComputerScientistAndrew@users.noreply.github.com>
2021-11-07 18:50:26 -0600

    Beginning product page

--- utils/styles.js
+++ utils/styles.js
@@ -13,6 +13,13 @@ const useStyles = makeStyles({
     },
     footer: {
         textAlign: 'center'
+    },
+    brand: {
+        fontWeight: 'bold',
+        fontSize: '1.5rem'
+    },
+    grow: {
+        flexGrow: 1
     }
 })
 
