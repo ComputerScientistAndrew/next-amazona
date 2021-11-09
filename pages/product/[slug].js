78a574ccc75df289f0d570563ca0d33957a72ca9
ComputerScientistAndrew <44506286+ComputerScientistAndrew@users.noreply.github.com>
2021-11-08 21:57:35 -0600

    Add to cart functionality and storing cart in cookies

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import Layout from '../../components/Layout';
@@ -6,12 +6,24 @@ import { Button, Card, Grid, Link, List, ListItem, Typography } from '@material-
 import useStyles from '../../utils/styles';
 import db from '../../utils/db';
 import Product from '../../models/Product';
+import axios from 'axios';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen(props) {
+    const { dispatch } = useContext(Store);
     const { product } = props;
     const classes = useStyles();
     if (!product) {
         return <div>Product Not Found</div>
+    }
+
+    const addToCartHandler = async () => {
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if (data.countInStock <= 0) {
+            window.alert('Sorry. Product is out of stock');
+            return;
+        }
+        dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
     }
     return (
         <Layout title={product.name} description={product.description}>
@@ -77,7 +89,8 @@ export default function ProductScreen(props) {
                                 </Grid>
                             </ListItem>
                             <ListItem>
-                                <Button fullWidth variant="contained" color="primary">
+                                <Button fullWidth variant="contained" color="primary"
+                                    onClick={addToCartHandler}>
                                     Add to cart
                                 </Button>
                             </ListItem>
